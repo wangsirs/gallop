082e8cf9a7791a1f417e95fa2ff1c922cf3daa4a
bs <bs@gmail.com>
2016-07-19 23:01:24 +0800

    1.sync add del row 2.fix bug and release

--- api/application/models/mt4/Base_model.php
+++ api/application/models/mt4/Base_model.php
@@ -23,6 +23,8 @@ class base_model extends CI_Model{
     public function sync_symbol(&$list){
         $this->db->trans_start();
         
+        $this->_del_old_symbol($list);
+        
         $this->_insert_symbol($list);
         
         $query = $query = $this->_chk_duplicat_symbol();
@@ -67,32 +69,69 @@ class base_model extends CI_Model{
      * @return array
      */
     public function chk_symbol(&$list){
+        $list_diff = array();
+        
         $this->db->trans_start();
         
+        //撈取準備刪除的資料
+        $list_symbol_id = array();
+        foreach($list as $row){
+            $list_symbol_id[] = $row['symbol_id'];
+        }
+        $this->db->where_not_in('mss_id', $list_symbol_id);
+        $this->db->where('expired', 0);
+        $query = $this->db->get(self::TB_MSS);
+        $list_diff = $query->result_array();
+        
+        $this->_del_old_symbol($list);
+        
+        $this->db->select('mss_id');
+        $this->db->where('expired', 0);
+        $query = $this->db->get(self::TB_MSS);
+        if($query->num_rows() > 0){
+            $list_exists = array();
+            foreach($query->result_array() as $row){
+                $list_exists[] = $row['mss_id'];
+            }
+            foreach($list as $symbol){
+                if( ! in_array($symbol['symbol_id'], $list_exists)){
+                    $list_diff[] = array('mss_id' => $symbol['symbol_id']);
+                }
+            }
+        }
+            
         $this->_insert_symbol($list);
         
         if($this->db->affected_rows() == 0){
-            return array();
+            return $list_diff;
         }
         
+        //取得重複資料中的正確資料
         $query = $this->_chk_duplicat_symbol();
-                
         $list_mss_id = array();
         if($query->num_rows() > 0){
             foreach($query->result_array() as $row){
                 $list_mss_id[] = $row['mss_id'];
             }
         }
+        //將資料回溯
         $this->db->trans_rollback();
         
-        $this->db->select('mss_id,symbol,security_group,ctime');
-        $this->db->where_in('mss_id', $list_mss_id);
-        $this->db->where_in('expired', 0);
-        
-        $query = $this->db->get(self::TB_MSS);
+        //取得排除正確資料的"重複資料)
+        if($query->num_rows() > 0){
+            $this->db->select('mss_id,symbol,security_group,ctime');
+            $this->db->where_in('mss_id', $list_mss_id);
+            $this->db->where_in('expired', 0);
 
-        
-        return $query->result_array();        
+            $query = $this->db->get(self::TB_MSS);
+
+            if($query->num_rows() > 0){
+                foreach($query->result_array() as $row){
+                    $list_diff[] = $row;
+                }
+            }
+        }
+        return $list_diff;
     }
     
     /**
@@ -124,5 +163,26 @@ class base_model extends CI_Model{
                 . ") mss WHERE mss.num > 1 ";
 //        
         return $this->db->query($sql);
+    }
+    
+    /**
+     * 刪除被刪掉的商品
+     * @param array $list
+     */
+    private function _del_old_symbol($list){
+        //處理被刪除的商品
+        $list_symbol_id = array();
+        foreach($list as $row){
+            $list_symbol_id[] = $row['symbol_id'];
+        }
+        
+        $this->db->where_not_in('mss_id', $list_symbol_id);
+        $this->db->where('expired', 0);
+
+
+        $this->db->set('expired', 1);
+        $this->db->set('extime', 'NOW()', FALSE);
+
+        $this->db->update(self::TB_MSS); 
     }
 }
