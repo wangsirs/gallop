7af28f1d8de8f92cfd8da5804cc13ab376ceca9e
wild <wild0522@gmail.com>
2016-07-26 00:40:45 +0800

    add & update table

---
+++ api/application/schema/mt4_ib_level.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.3
+-- https://www.phpmyadmin.net/
+--
+-- 主機: localhost
+-- 產生時間： 2016 年 07 月 26 日 00:36
+-- 伺服器版本: 5.7.13
+-- PHP 版本： 7.0.8-3+deb.sury.org~trusty+1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `gallop_dev`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `mt4_ib_level`
+--
+
+CREATE TABLE `mt4_ib_level` (
+  `mil_id` int(3) UNSIGNED NOT NULL COMMENT '方案編號',
+  `mil_name` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '方案名稱',
+  `mil_level` smallint(3) UNSIGNED NOT NULL DEFAULT '0' COMMENT '等級',
+  `mil_title` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '職級稱號',
+  `mil_scale` smallint(3) UNSIGNED NOT NULL DEFAULT '0' COMMENT '佣金比例',
+  `ctime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '新建日期',
+  `utime` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新時間',
+  `extime` datetime NOT NULL COMMENT '刪除時間',
+  `expired` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否刪除'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='MT4顧問層級';
+
+--
+-- 已匯出資料表的索引
+--
+
+--
+-- 資料表索引 `mt4_ib_level`
+--
+ALTER TABLE `mt4_ib_level`
+  ADD PRIMARY KEY (`mil_id`);
+
+--
+-- 在匯出的資料表使用 AUTO_INCREMENT
+--
+
+--
+-- 使用資料表 AUTO_INCREMENT `mt4_ib_level`
+--
+ALTER TABLE `mt4_ib_level`
+  MODIFY `mil_id` int(3) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '方案編號';
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- api/application/schema/mt4_symbol_plan.sql
+++ api/application/schema/mt4_symbol_plan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機: localhost
--- 產生時間： 2016 年 07 月 20 日 20:21
+-- 產生時間： 2016 年 07 月 26 日 00:20
 -- 伺服器版本: 5.7.13
 -- PHP 版本： 7.0.8-3+deb.sury.org~trusty+1
 
@@ -27,18 +27,26 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `mt4_symbol_plan` (
-  `msp_id` varchar(16) UNSIGNED NOT NULL COMMENT 'MT4商品編號',
+  `msp_seq` int(8) UNSIGNED NOT NULL COMMENT '流水號',
+  `msp_id` varchar(16) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'MT4商品編號',
   `security_group` varchar(16) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '商品群組',
   `msp_scale` double UNSIGNED NOT NULL DEFAULT '0' COMMENT '佣金',
   `msp_spread` double UNSIGNED NOT NULL DEFAULT '0' COMMENT '點差',
   `msp_volume_min` int(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '最小交易量',
   `msp_volume_max` int(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '最大交易量',
+  `msp_private` tinyint(1) UNSIGNED NOT NULL DEFAULT '0' COMMENT '是否為自訂',
+  `msp_abook` tinyint(1) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'ABook(0=未設定,1=拋上手,2=無需A)',
   `ctime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '新建日期',
   `utime` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新時間',
   `extime` datetime NOT NULL COMMENT '刪除時間',
   `expired` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否刪除'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='MT4佣金點差方案';
 
+--
+-- 使用資料表 AUTO_INCREMENT `mt4_symbol_plan`
+--
+ALTER TABLE `mt4_symbol_plan`
+  MODIFY `msp_seq` int(8) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '流水號', AUTO_INCREMENT=1;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
